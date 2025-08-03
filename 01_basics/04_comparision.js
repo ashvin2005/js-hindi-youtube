187fab93eb9f9c3c2cc5e1c9a40e9a61dab319e6
Ashvin Tiwari <ashvintiwari161@gmail.com>
2025-08-03 15:24:53 +0530

    null

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -12,6 +12,9 @@ console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
 
+// comparison operator convert null to 0.
+
+
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
